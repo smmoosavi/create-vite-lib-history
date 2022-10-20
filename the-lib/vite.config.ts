662f6d79e5ffb5ace62bcb5873828863ff5df142
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2022-10-20 21:41:36 +0330

    create-vite-lib v0.2.1

--- the-lib/vite.config.ts
+++ the-lib/vite.config.ts
@@ -14,8 +14,8 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      formats: ['es'],
-      fileName: 'index',
+      formats: ['es', 'cjs'],
+      fileName: (format) => (format === 'es' ? 'index.mjs' : 'index.cjs'),
     },
     rollupOptions: {
       external,
