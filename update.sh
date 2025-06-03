587b3d2dc2a030b255fdf8a978e08df60ce71d8c
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2025-06-04 01:03:21 +0330

    add create-vite@6.5.0/react-ts

--- update.sh
+++ update.sh
@@ -1,6 +1,13 @@
 #/bin/bash
 rm -rf the-lib
 rm -rf the-react-lib
+rm -rf the-vite-app
+
+PARENT_DIR=$(dirname "$(realpath "$0")")
 
 ../create-vite-lib/index.js lib the-lib 2>&1 | tee lib-output
 ../create-vite-lib/index.js react-lib the-react-lib 2>&1 | tee react-lib-output
+
+echo "create-vite@$(pnpm info create-vite version )\n" | tee vite-app-output
+pnpm create vite the-vite-app --template react-ts 2>&1 |  sed "s|${PARENT_DIR}|.|g" |tee -a vite-app-output
+
