7098443af13f53d6d4921a6d28e53e5aa070d34d
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2025-06-05 12:57:49 +0330

    add create-vite@6.5.0/vanilla-ts

--- update.sh
+++ update.sh
@@ -1,7 +1,8 @@
 #/bin/bash
 rm -rf the-lib
 rm -rf the-react-lib
-rm -rf the-vite-app
+rm -rf vite-vanilla-app
+rm -rf vite-react-app
 
 PARENT_DIR=$(dirname "$(realpath "$0")")
 
@@ -9,5 +10,12 @@ PARENT_DIR=$(dirname "$(realpath "$0")")
 ../create-vite-lib/index.js react-lib the-react-lib 2>&1 | tee react-lib-output
 
 echo "create-vite@$(pnpm info create-vite version )\n" | tee vite-app-output
-pnpm create vite the-vite-app --template react-ts 2>&1 |  sed "s|${PARENT_DIR}|.|g" |tee -a vite-app-output
+
+# template-vanilla-ts
+echo "--- vanilla-ts ---\n" | tee -a vite-app-output
+pnpm create vite vite-vanilla-app --template vanilla-ts 2>&1 | sed "s|${PARENT_DIR}|.|g" | tee -a vite-app-output
+
+# template-react-ts
+echo "--- react-ts ---\n" | tee -a vite-app-output
+pnpm create vite vite-react-app --template react-ts 2>&1 |  sed "s|${PARENT_DIR}|.|g" |tee -a vite-app-output
 
